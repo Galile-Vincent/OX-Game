74fe528be249254ff551f8577fa7ab1c9664dac8
Vincent <vincent.ua.248@gmail.com>
2023-10-07 22:41:26 +0800

    Create  vincent_gameApp.swift

---
+++ vincent_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  vincent_gameApp.swift
+//  vincent game
+//
+//  Created by Vincent Chiang on 2022/3/3.
+//
+
+import SwiftUI
+
+@main
+struct vincent_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameView()
+        }
+    }
+}
